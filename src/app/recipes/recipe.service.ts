63ae11d2aea3167eb19f5d9775515b079dd020a4
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-03-12 14:26:27 +0500

    Add service for cross-comp communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A test Recipe', 'This is simply a test', 'https://www.maggi.co.uk/sites/default/files/styles/maggi_desktop_image_style/public/NUK1265%20maggi%20Recipes%20banner%201500x700px%20opt2A.jpg?h=4f5b30f1&itok=DcsF1RwA'),
     new Recipe('A another Recipe', 'This is another test', 'https://www.washingtonpost.com/resizer/GhgmkKBkqVEKUwQj9tNT-0OtVAc=/arc-anglerfish-washpost-prod-washpost/public/L5RAEGUUZQI6ZOZROT6ANQFDUU.jpg')
